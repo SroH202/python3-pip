641fa2ec3b0cdf7ffffac4a18649b4f206dc413c
Nelson Bonilla <nelsonbonillabestene@gmail.com>
2023-05-26 20:39:40 -0500

    Se mantiene un error en la realización de las gráficas

--- app/main.py
+++ app/main.py
@@ -3,7 +3,9 @@ import read_csv
 import charts
 
 def run():
+ 
   continent_user = input('¿De qué continente desea conocer la información? ==> ')
+ 
   data = read_csv.read_csv('data.csv')
   
   data = list(filter(lambda item : item['Continent'] == continent_user,data))
@@ -12,16 +14,16 @@ def run():
   percentages = list(map(lambda x: x['World Population Percentage'], data))
   charts.generate_pie_chart(countries, percentages)
   
-  
+  '''
   country = input('Type Country => ')
-
+  
   result = utils.population_by_country(data, country)
 
   if len(result) > 0:
     country = result[0]
     labels, values = utils.get_population(country)
     charts.generate_bar_chart(labels, values)
-  
+  '''
 
 # Dualidad en python
 if __name__ == '__main__':
